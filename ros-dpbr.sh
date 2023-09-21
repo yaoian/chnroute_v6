357b16481ce4f6d691600d108b43679cb3bb26a9
Asai <yaoian@outlook.com>
2023-09-21 23:50:04 +0800

    ipv6

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,16 +3,16 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O CN_v6.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute_v6.txt
 
 {
 echo "/ip firewall address-list"
 
-for net in $(cat CN.txt) ; do
+for net in $(cat CN_v6.txt) ; do
   echo "add list=CN address=$net comment=AS4809"
 done
 
-} > ../CN.rsc
+} > ../CN_v6.rsc
 
 cd ..
 rm -rf ./pbr
